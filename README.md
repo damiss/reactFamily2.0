0500c0faa5b57bfcd85b50bbc215ef8ca17f2b30
王军-PC\王军 <296188842@qq.com>
2017-11-16 17:31:03 +0800

    reactFamily2.0重构版

---
+++ README.md
@@ -0,0 +1,1 @@
+# reactFamily2.0
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// document.getElementById('app').innerHTML = "webpack works";
+
+/* const func = (str) => {
+    document.getElementById('app').innerHTML = str
+}
+func('我是经过babel编译过的!!!') */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './component/Hello/Hello'
+import {AppContainer} from 'react-hot-loader'
+
+import getRouter from './router/router'
+
+/* 初始化 */
+renderWithHotReload(getRouter());
+
+/* 热更新 */
+if(module.hot) {
+    module.hot.accept('./router/router', () => {
+        const getRouter = require('./router/router').default;
+        renderWithHotReload(getRouter());
+    });
+}
+
+function renderWithHotReload(RootElement) {
+    ReactDOM.render(
+        <AppContainer>
+            {RootElement}
+        </AppContainer>,
+        document.getElementById('app')
+    )
+}
+
